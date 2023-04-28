6f0aaa959ff972366e72da4d6c82aeacdf03b1c0
j1mmyto9 <khanhuitse05@gmail.com>
2023-04-28 19:04:16 +0700

    feat: force update

--- lib/src/features/dashboard/logic/dashboard_bloc.dart
+++ lib/src/features/dashboard/logic/dashboard_bloc.dart
@@ -1,9 +1,22 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:myapp/src/config/constants/navigation_bar_item.dart';
+import 'package:myapp/src/dialogs/alert_wrapper.dart';
 import 'package:myapp/src/router/coordinator.dart';
+import 'package:myapp/src/services/remote_config/remote_config_service.dart';
 
 class DashboardBloc extends Cubit<XNavigationBarItems> {
-  DashboardBloc(XNavigationBarItems current) : super(current);
+  DashboardBloc(XNavigationBarItems current) : super(current) {
+    checkForceUpdate();
+  }
+
+  Future checkForceUpdate() async {
+    final needForceUpdate = RemoteConfigService.config.needForceUpdate;
+    if (needForceUpdate) {
+      XAlert.showForceUpdate();
+      return true;
+    }
+    return false;
+  }
 
   void onDestinationSelected(int index) {
     emit(XNavigationBarItems.values[index]);
