64dea6dc4b1a604760514d364eae33f3391b9064
jimmy-to-goldenowl <jimmy.to.goldenowl@gmail.com>
2023-08-04 16:32:18 +0700

    remove unnecessary package

--- lib/src/network/blob/data/upload_repository_impl.dart
+++ lib/src/network/blob/data/upload_repository_impl.dart
@@ -1,10 +1,8 @@
 // ignore: depend_on_referenced_packages
 import 'package:http_parser/http_parser.dart' as http_parser;
-import 'package:photo_manager/photo_manager.dart';
 import '../../model/common/result.dart';
 import '../model/upload_model.dart';
 import 'upload_repository.dart';
-import 'file_type_helper.dart';
 
 class UploadRepositoryImpl extends UploadRepository {
   // Submit content must be image, video or audio
@@ -17,12 +15,12 @@ class UploadRepositoryImpl extends UploadRepository {
 
   @override
   Future<MResult<MUpload>> uploadImage(String path) async {
-    return uploadFile(path, FileTypeHelper.mediaTypeImage);
+    return uploadFile(path, http_parser.MediaType('image', 'png'));
   }
 
   @override
   Future<MResult<MUpload>> uploadVideo(String path) async {
-    return uploadFile(path, FileTypeHelper.mediaTypeVideo);
+    return uploadFile(path, http_parser.MediaType('video', 'mp4'));
   }
 
   @override
@@ -33,21 +31,5 @@ class UploadRepositoryImpl extends UploadRepository {
   @override
   Future<List<MResult<MUpload>>> uploadVideos(List<String> paths) {
     return Future.wait([for (final e in paths) uploadVideo(e)]);
-  }
-
-  @override
-  Future<MResult<MUpload>> uploadAssetFile(AssetEntity asset) async {
-    final file = await asset.file;
-    if (file != null) {
-      http_parser.MediaType contentType =
-          FileTypeHelper.mediaTypeFromAssetType(asset.type);
-      return uploadFile(file.path, contentType);
-    }
-    return MResult.error('File not found');
-  }
-
-  @override
-  Future<List<MResult<MUpload>>> uploadAssetFiles(List<AssetEntity> assets) {
-    return Future.wait([for (final e in assets) uploadAssetFile(e)]);
   }
 }
